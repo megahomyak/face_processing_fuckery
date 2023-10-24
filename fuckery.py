56d180c27bc68dea358000e7cdea72751045c885
megahomyak <g.megahomyak@gmail.com>
2023-10-24 11:37:09 +0500

    Trying to get the most stable emotion

--- fuckery.py
+++ fuckery.py
@@ -1,16 +1,25 @@
 from deepface import DeepFace
 import cv2
 
-camera = cv2.VideoCapture()
+camera = cv2.VideoCapture(0)
 
 while True:
     ret, frame = camera.read()
     if not ret:
         break
-    analyzed = DeepFace.analyze(
-        frame,
-        actions=("emotion",)
-    )
-
-    print(analyzed[0]["dominant_emotion"])
-    break
+    try:
+        analyzed = DeepFace.analyze(
+            frame,
+            actions=("emotion",)
+        )
+    except ValueError:
+        pass
+    else:
+        emotions = analyzed[0]["emotion"]
+        print(emotions)
+        for emotion_name, emotion_value in emotions.items():
+            if emotion_value > 60:
+                print(emotion_name)
+            break
+        else:
+            print("No dominant emotion")
